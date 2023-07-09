bc4c0d1b05d1f1802dc440afbd772e7befdae861
HSDrummond <hsdrum1@gmail.com>
2023-07-08 23:08:29 -0700

    Counter Goes brr

--- Assets/CollisionHandler.cs
+++ Assets/CollisionHandler.cs
@@ -1,13 +1,20 @@
+using System;
 using UnityEngine;
 
 public class CollisionHandler : MonoBehaviour
 {
+    public static event Action SlimeKilled = delegate { };
+
     public GameObject visualEffect; // Assign the visual effect prefab in the Inspector
 
     private void OnTriggerEnter(Collider collision)
     {
         if (collision.gameObject.CompareTag("Sword"))
         {
+            if (gameObject.CompareTag("Slime"))
+            {
+                SlimeKilled();
+            }
             PlayVisualEffect();
             Destroy(gameObject);
         }
@@ -17,7 +24,8 @@ public class CollisionHandler : MonoBehaviour
     {
         if (visualEffect != null)
         {
-            Instantiate(visualEffect, transform.position, Quaternion.identity);
+            GameObject vfxInstance = Instantiate(visualEffect, transform.position, Quaternion.identity);
+            Destroy(vfxInstance, 1f);
         }
     }
 }
